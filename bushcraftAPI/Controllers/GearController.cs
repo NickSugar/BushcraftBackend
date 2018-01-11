4168a815081c9e4667f4ad6dcac7203b6d513571
NickSugar <nicksugar1@outlook.com>
2018-01-11 14:19:48 -0800

    added gearController and route

---
+++ bushcraftAPI/Controllers/GearController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bushcraftAPI.Controllers
+{
+    [Route("/[controller]")]
+    public class GearController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet(Name = nameof(GetGear))]
+        public IActionResult GetGear()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

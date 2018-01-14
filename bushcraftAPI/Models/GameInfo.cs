54eed1f1db8d285856a28a5e9d093ff7c51d3c09
NickSugar <nicksugar1@outlook.com>
2018-01-13 18:52:34 -0800

    started resource base class and gameInfo resource

---
+++ bushcraftAPI/Models/GameInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bushcraftAPI.Models
+{
+    public class GameInfo : Resource
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string DesignerName { get; set; }
+        public ICollection<Designer> Designers { get; set; } 
+    }
+
+    public class Designer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
